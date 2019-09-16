9cd5313c371295bb753197ce5ee3696b5e95faae
Verdi Pratama <verdipratama@icloud.com>
2019-09-16 18:56:10 +0700

    Initial Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { graphql, StaticQuery, Link } from 'gatsby'
+
+import * as styles from './style'
+
+export function Header() {
+    return (
+        <StaticQuery
+            query={query}
+            render={({ site }) => (
+                <div css={styles.headerWrapper}>
+                    <Link to="/">{site.siteMetadata.author}</Link>
+                    <div css={styles.containerBtnLink}>
+                        <Link to="/blog">blog</Link>
+                    </div>
+                </div>
+            )}
+        />
+    )
+}
+
+const query = graphql`
+    query {
+        site {
+            siteMetadata {
+                author
+            }
+        }
+    }
+`
---
+++ src/configs/constants.js
@@ -0,0 +1,1 @@
+export const IS_MOBILE = '(max-width: 1024px)'
