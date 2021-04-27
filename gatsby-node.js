112f37a03531c0936a84ee568b0f9025b31fd34e
verdipratama <verdipratama@icloud.com>
2021-04-27 15:49:13 +0700

    rapihkan

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,6 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
   return graphql(
     `
