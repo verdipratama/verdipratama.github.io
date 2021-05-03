f93c6342f60cdd714c59b1e2efdd968331ff9ff3
verdipratama <verdipratama@icloud.com>
2021-05-03 16:29:44 +0700

    rapihkan

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,7 +35,6 @@ exports.createPages = ({ graphql, actions }) => {
     posts.forEach((post, index) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1].node
       const next = index === 0 ? null : posts[index - 1].node
-
       createPage({
         path: post.node.fields.slug,
         component: blogPost,
@@ -53,7 +52,6 @@ exports.createPages = ({ graphql, actions }) => {
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
     createNodeField({
